c27e3617ea6670128a53683d5782d5cc15a8a7cf
Martin Høite <martinhblizz@gmail.com>
2019-02-17 19:23:00 +0100

    A lot of testing included, REALLY need to clean up the project.

--- js/MainJS.js
+++ js/MainJS.js
@@ -4,31 +4,40 @@ $(document).ready(function () {
     $('.materialboxed').materialbox();
     //Initialize tooltips
     $('.tooltipped').tooltip();
+    //Initialize sidenav for mobile
+    // $('.sidenav').sidenav();
 
     //#endregion Initialization
 
     //#region Video and Audio controls
-    let mainVideo = $('#mainVideo').get(0);
+    let mainVideo = $('#mainVideo');
+    let mainVideoControl = mainVideo.get(0);
     let mainVolume = 0.5;
     let volumeMuteState = false;
     let audioMuteBtn = $('#audioMuteBtn');
     let autoPlay = false;
     let autoPlayBtn = $('#autoPlayBtn');
+    let pages = [0,1,2,3,4,5,6,7,8,9,10];
 
-    mainVideo.volume = mainVolume;
-    $('#mainVideo').on('click', function (e) {
-        if (mainVideo.paused) {
-            mainVideo.play();
+    mainVideoControl.onended = function() {
+        // mainVideo.hide("slow");
+        mainVideo.hide("slide", { direction: "left" }, 1000);
+    };
+
+    mainVideoControl.volume = mainVolume;
+    mainVideo.on('click', function (e) {
+        if (mainVideoControl.paused) {
+            mainVideoControl.play();
         }
         else {
-            mainVideo.pause();
+            mainVideoControl.pause();
         }
     });
 
     //Let the audioSlider control all audio
     $('#audioSlider').on('input', function (e) {
         // console.log($(this).val() / 100);
-        mainVideo.volume = $(this).val() / 100;
+        mainVideoControl.volume = $(this).val() / 100;
         mainVolume = $(this).val() / 100;
 
         //Slight hack to trigger a reset of the mute state (and updating the button to show the change)
@@ -39,13 +48,13 @@ $(document).ready(function () {
 
     audioMuteBtn.on('click', function (e) {
         if (volumeMuteState) {
-            mainVideo.volume = mainVolume;
+            mainVideoControl.volume = mainVolume;
             $('#audioSlider').val = mainVolume;
             volumeMuteState = false;
             //Update icon to fit new state
             audioMuteBtn.html('<i class="material-icons">volume_up</i>');
         } else {
-            mainVideo.volume = 0;
+            mainVideoControl.volume = 0;
             $('#audioSlider').val = mainVolume;
             volumeMuteState = true;
             //Update icon to fit new state
